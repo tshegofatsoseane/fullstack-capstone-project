56e1f1e0ac12fe5968ad74f235b47b692c84970a
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2025-09-05 05:43:53 -0400

    update

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,7 @@ import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
